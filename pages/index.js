a3ba2ec3a3bd63b2e8e4b69e88f07c284f31a40a
Shemona Singh <singhshemona@gmail.com>
2021-09-30 17:04:02 -0400

    made a scrollable section

--- pages/index.js
+++ pages/index.js
@@ -4,23 +4,22 @@ import styles from "./index.module.css";
 
 export const databaseId = process.env.NOTION_DATABASE_ID;
 
-export default function Home({ summary, content, source }) {
+export default function Home({ summary, content, source, emoji }) {
   return (
     <div className={styles.container}>
       <Head>
         <title>Spark</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className={styles.title}>Today's selection</h1>
       <main className={styles.quote}>
         <p className={styles.summary}>{summary}</p>
         <div className={styles.content}>
           {content.map((paragraph, index) => <p key={index}>{paragraph}</p>)}
         </div>
-        <p>From: {source}</p>
+        <p className={styles.source}><span className={styles.sourceTitle}>Source:</span> {emoji} {source}</p>
       </main>
       <button className={styles.button} onClick={() => {window.location.href = window.origin}}>Generate New Note</button>
-      <a className={styles.link} target="_blank" href='https://www.notion.so/35de7cb65366432eb56d815a97a4767e' className={styles.heading}>Head to Slip-Box</a>
+      <a className={styles.link} target="_blank" href='https://www.notion.so/35de7cb65366432eb56d815a97a4767e' className={styles.heading}>Slip-Box</a>
     </div>
   );
 }
@@ -46,13 +45,16 @@ export const getStaticProps = async () => {
 
   const quotePage = await getPage(blockId)
   const sourcePage = await getPage(quotePage.properties.Source.relation[0].id)
+
+  const emoji = sourcePage.icon.emoji
   const source = sourcePage.properties.Title.title[0].plain_text
 
   return {
     props: {
-      summary: summary,
       content: content,
-      source: source
+      emoji: emoji,
+      source: source,
+      summary: summary
     },
     revalidate: 1,
   };
